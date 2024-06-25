30a8e1d6ae3da144445f8f109c87a0317d7bd5d2
Ywoosang <opellong13@gmail.com>
2024-06-26 07:01:48 +0900

    initial commit

---
+++ src/main/java/com/ywoosang/tech/api/BlogServerApplication.java
@@ -0,0 +1,13 @@
+package com.ywoosang.tech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ywoosang/tech/api/BlogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywoosang.tech.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
