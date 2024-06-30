885d41471fc8ff6b1d387721e94f7483ecaeb0ce
Ywoosang <opellong13@gmail.com>
2024-07-01 03:01:03 +0900

    fix: slug 기반 삭제가 가능하도록 deleteBySlug 수정 - @Modifying, @Query 이용

--- blog-domain/src/main/java/com/ywoosang/tech/domains/post/repository/PostRepository.java
+++ blog-domain/src/main/java/com/ywoosang/tech/domains/post/repository/PostRepository.java
@@ -2,9 +2,15 @@ package com.ywoosang.tech.domains.post.repository;
 
 import com.ywoosang.tech.domains.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
+
+    @Modifying
+    @Query("DELETE FROM Post p WHERE p.slug = :slug")
     void deleteBySlug(String slug);
 }
